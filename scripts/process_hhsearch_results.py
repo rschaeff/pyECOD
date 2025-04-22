3d939c49c7a361d4d77ae0f1b1f3d79259966ea5
R. Dustin Schaeffer <ZoroasterD@gmail.com>
2025-04-22 12:54:03 -0700

    Update process_hhsearch_results.py

--- scripts/process_hhsearch_results.py
+++ scripts/process_hhsearch_results.py
@@ -14,7 +14,7 @@ from datetime import datetime
 sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 
 from ecod.core.context import ApplicationContext
-from ecod.pipelines.hhsearch import HHRToXMLConverter, HHSearchProcessor
+from ecod.pipelines.hhsearch import HHRToXMLConverter, HHSearchProcessor, DomainEvidenceCollator
 from ecod.utils.hhsearch_utils import HHRParser
 
 def setup_logging(verbose=False, log_file=None):
