4ab72eccddd5321d16c3ef9ee832b59830e7a512
R. Dustin Schaeffer <ZoroasterD@gmail.com>
2025-05-02 16:40:13 -0700

    Update partition.py

--- ecod/pipelines/domain_analysis/partition.py
+++ ecod/pipelines/domain_analysis/partition.py
@@ -17,8 +17,6 @@ from ecod.core.context import ApplicationContext
 from ecod.db import DBManager
 from ecod.models.pipeline import BlastHit, HHSearchHit, DomainSummaryModel, PipelineResult
 from ecod.models.domain_analysis import DomainPartitionResult
-from ecod.models.domain_analysis.domain_candidate import DomainCandidate
-from ecod.models.domain import Domain, DomainRange, DomainRangeSegment
 from ecod.utils.xml_utils import ensure_dict, ensure_list_of_dicts
 from ecod.utils.path_utils import get_standardized_paths, get_all_evidence_paths, resolve_file_path
 from ecod.utils.file import find_fasta_file, read_sequence_from_fasta
