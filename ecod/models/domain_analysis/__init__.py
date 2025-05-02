65baa18fbccd93ae5202f3b7c42b6d1b518b8612
R. Dustin Schaeffer <ZoroasterD@gmail.com>
2025-05-02 16:25:34 -0700

    more model changes

--- ecod/models/domain_analysis/__init__.py
+++ ecod/models/domain_analysis/__init__.py
@@ -1,4 +1,5 @@
 from .partition_result import DomainPartitionResult
 from .domain_candidate import DomainCandidate
+from .domain_model import DomainModel
 
-__all__ = ["DomainPartitionResult", "DomainCandidate"]
+__all__ = ["DomainPartitionResult", "DomainCandidate", "DomainModel"]
